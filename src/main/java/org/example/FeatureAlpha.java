de0a80b53a35d73f4638aa1aad453a67cd467a1a
Alberto Garcia Sanchez <alberto.sanchez@ecs.co.uk>
2023-03-02 15:30:14 +0000

    [TicketNumber] Adding implementation for feature_alpha

---
+++ src/main/java/org/example/FeatureAlpha.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class FeatureAlpha {
+
+    public FeatureAlpha(){}
+
+    public void run(){
+        System.out.println("This is feature alpha");
+        System.out.println("This is a bug");
+    }
+}
