50430a00b8c4c1c154300b03e66b74a78e8bee2f
Alberto Garcia Sanchez <alberto.sanchez@ecs.co.uk>
2023-03-02 15:49:21 +0000

    [TicketNumber] Integrating feature_beta to the application.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        FeatureBeta beta = new FeatureBeta();
+        beta.run();
     }
 }
